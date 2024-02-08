04d98ef3afa99693c811a96140af06c38d951d68
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2024-02-07 22:52:53 -0300

    Init Commit

---
+++ src/main/java/com/marcosoft/marcosoftcustomerexample/MarcosoftCustomerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.marcosoft.marcosoftcustomerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcosoftCustomerExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarcosoftCustomerExampleApplication.class, args);
+    }
+
+}
