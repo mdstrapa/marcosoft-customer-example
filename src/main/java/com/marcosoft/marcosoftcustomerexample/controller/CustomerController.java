5609fad3f233566672fb79a1e30acd148f198b59
Marcos Daniel Strapazon <marcos_strapazon@sicredi.com.br>
2024-02-08 22:13:51 -0300

    feat: findAll operation working

--- src/main/java/com/marcosoft/marcosoftcustomerexample/controller/CustomerController.java
+++ src/main/java/com/marcosoft/marcosoftcustomerexample/controller/CustomerController.java
@@ -5,10 +5,9 @@ import com.marcosoft.marcosoftcustomerexample.service.CustomerService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("customer")
@@ -20,6 +19,27 @@ public class CustomerController {
     @PostMapping
     public ResponseEntity<Customer> save(@RequestBody Customer newCustomer){
         return ResponseEntity.status(HttpStatus.CREATED).body(customerService.save(newCustomer));
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Customer> findById(@PathVariable Long id){
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.findById(id));
+    }
+
+    @GetMapping("all")
+    public ResponseEntity<List<Customer>> findAll(){
+        return ResponseEntity.status(HttpStatus.OK).body(customerService.findAll());
+    }
+
+    @PutMapping()
+    public ResponseEntity<Customer> update(@RequestBody Customer customer){
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(customerService.update(customer));
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> delete(@PathVariable Long id){
+        customerService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("");
     }
 
 }
--- src/main/java/com/marcosoft/marcosoftcustomerexample/service/CustomerService.java
+++ src/main/java/com/marcosoft/marcosoftcustomerexample/service/CustomerService.java
@@ -3,10 +3,27 @@ package com.marcosoft.marcosoftcustomerexample.service;
 import com.marcosoft.marcosoftcustomerexample.model.Customer;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class CustomerService {
 
     public Customer save(Customer newCustomer){
         return null;
+    }
+
+    public Customer findById(Long id){
+        return null;
+    }
+
+    public List<Customer> findAll(){
+        return null;
+    }
+
+    public Customer update(Customer customer){
+        return null;
+    }
+
+    public void delete(Long id){
     }
 }
